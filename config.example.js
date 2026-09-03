2abc30a240f17953e8ef92aada8e2219013d136a
Kevin Ng <kevinnggg@gmail.com>
2026-09-03 12:53:49 -0700

    Clean up

--- config.example.js
+++
@@ -1,5 +0,0 @@
-window.APP_CONFIG = {
-  publicSheetUrl: 'https://docs.google.com/spreadsheets/d/REPLACE_WITH_PUBLIC_SHEET_ID/export?format=csv&gid=0',
-  adminUsername: 'admin',
-  adminPassword: 'admin123'
-};
